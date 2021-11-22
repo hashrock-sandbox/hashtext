794f43ea3eeb56138a122c698b11e7ae3703792a
hashrock <hashrock@users.noreply.github.com>
2021-11-22 15:28:44 +0900

    feat: dropfile

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,52 +1,59 @@
 ﻿using Microsoft.Win32;
 using Reactive.Bindings;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace hash_textarea
 {
     class ViewModel : INotifyPropertyChanged
     {
+        private const string _pattern = "テキストファイル(*.txt)|*.txt|CSVファイル(*.csv)|*.csv|全てのファイル(*.*)|*.*";
+
         public ReactiveProperty<string> Content
         {
             get; set;
         }
+        public ReactiveProperty<string?> Path
+        {
+            get; set;
+        }
+        public ReactiveProperty<string> Title
+        { get; set; }
 
         public ViewModel()
         {
             Content = new ReactiveProperty<string>("");
+            Path = new ReactiveProperty<string?>();
+            Title = Path.Select(i =>
+            {
+                return i != null ? "HashText - " + i : "HashText - New File";
+            }).ToReactiveProperty<string>();
+
+            //Title = Path.Value != null ? "HashText" + Path.Value : "HashText";
+
             SaveFileCommand.Subscribe(_ =>
             {
-                var dialog = new SaveFileDialog();
-                dialog.Filter = "テキストファイル(*.txt)|*.txt|CSVファイル(*.csv)|*.csv|全てのファイル(*.*)|*.*";
-                var result = dialog.ShowDialog() ?? false;
-
-                if (!result)
+                if (Path.Value != null)
                 {
-                    return;
+                    File.WriteAllText(Path.Value, Content.Value);
                 }
-
-                File.WriteAllText(dialog.FileName, Content.Value);
+                else
+                {
+                    SaveNewFile();
+                }
+            });
+            SaveFileAsCommand.Subscribe(_ =>
+            {
+                SaveNewFile();
             });
 
             OpenFileCommand.Subscribe(_ =>
             {
                 var dialog = new OpenFileDialog();
-                dialog.Filter = "テキストファイル(*.txt)|*.txt|CSVファイル(*.csv)|*.csv|全てのファイル(*.*)|*.*";
+                dialog.Filter = _pattern;
 
                 var result = dialog.ShowDialog() ?? false;
 
@@ -55,12 +62,30 @@ namespace hash_textarea
                     return;
                 }
                 Content.Value = File.ReadAllText(dialog.FileName);
+                Path.Value = dialog.FileName;
             });
+
         }
 
+        private void SaveNewFile()
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = _pattern;
+            var result = dialog.ShowDialog() ?? false;
+
+            if (!result)
+            {
+                return;
+            }
+
+            File.WriteAllText(dialog.FileName, Content.Value);
+            Path.Value = dialog.FileName;
+        }
+
+        public ReactiveCommand SaveFileAsCommand { get; } = new ReactiveCommand();
         public ReactiveCommand SaveFileCommand { get; } = new ReactiveCommand();
         public ReactiveCommand OpenFileCommand { get; } = new ReactiveCommand();
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 
 
@@ -76,6 +101,38 @@ namespace hash_textarea
             InitializeComponent();
             vm = new ViewModel();
             DataContext = vm;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            string[]? files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files == null)
+            {
+                return;
+            }
+            // 1つだけファイルを開く
+            if (File.Exists(files[0]) == false)
+            {
+                return;
+            }
+            string filePath = files[0];
+            string fullPath = Path.GetFileName(filePath);
+
+            vm.Path.Value = fullPath;
+            vm.Content.Value = File.ReadAllText(filePath);
+        }
+
+        private void Window_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
         }
     }
 }
